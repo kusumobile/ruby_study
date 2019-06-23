816aee99dc79203d1a57a83e775008b69930eb68
RyotaroKusumoto <kusumobile@gmail.com>
2019-06-23 20:46:26 +0900

    Update and rename Bday.rb to Weekday.rb

--- ruby_study/Weekday.rb
+++ ruby_study/Weekday.rb
@@ -2,7 +2,6 @@
 i=0
 while i<30 do
   day = Time.new(2019,06,1) + 60 * 60 * 24 * i
-  puts day.strftime("%Y-%m-%d")
-  puts day.strftime("%Y-%m-%d")
+  puts day.strftime("%Y-%m-%d") unless day.sunday? || day.saturday?
   i += 1
 end
